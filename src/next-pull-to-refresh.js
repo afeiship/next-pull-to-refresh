0e0d1ae19ea776bf05777ce8cdc86a75cb7221ec
afeiship <1290657123@qq.com>
2018-09-18 23:31:32 +0800

    issue: opmtize

--- src/next-pull-to-refresh.js
+++ src/next-pull-to-refresh.js
@@ -8,7 +8,6 @@
     instructionsPullToRefresh: '下拉刷新',
     instructionsReleaseToRefresh: '释放更新',
     instructionsRefreshing: '数据加载中',
-    refreshTimeout: 1000,
     distReload: 64,
     getStyles: function () {
       return '.ptr--ptr { border-bottom:0.55px solid #eee; pointer-events: none; font-size: 0.85em; font-weight: bold; top: 0; height: 0; transition: height 0.3s, min-height 0.3s; text-align: center; width: 100%; overflow: hidden; display: flex; align-items: flex-end; align-content: stretch; } .ptr--box { padding: 10px; flex-basis: 100%; } .ptr--pull { transition: none; } .ptr--text { margin-top: 0.33em; color: rgba(0, 0, 0, 0.3); } .ptr--icon { color: rgba(0, 0, 0, 0.3); transition: transform 0.3s; } .ptr--top { touch-action: pan-x pan-down pinch-zoom; } .ptr--release .ptr--icon { transform: rotate(180deg); }';
